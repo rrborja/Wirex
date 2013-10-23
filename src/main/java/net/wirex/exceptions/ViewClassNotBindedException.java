e4b53d33c730f3e95fb6cd44f531f23889ba6b73
RBORJA <RBORJA@RITCHIEBORJA@GMAIL.COM>
2013-10-23 10:59:33 -0700

    Created new exception in checking if a view class is annotated with @Bind

---
+++ src/main/java/net/wirex/exceptions/ViewClassNotBindedException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.wirex.exceptions;
+
+/**
+ *
+ * @author RBORJA
+ */
+public class ViewClassNotBindedException extends Exception {
+
+    public ViewClassNotBindedException(String message) {
+        super(message);
+    }
+    
+}
