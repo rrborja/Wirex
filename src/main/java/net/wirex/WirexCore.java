bb23aed17587dfcb0223e85ffa12a3ab02aedc5a
Ritchie <ritchieborja@gmail.com>
2014-02-01 07:41:51 +0800

    version fix update 49

--- src/main/java/net/wirex/WirexCore.java
+++ src/main/java/net/wirex/WirexCore.java
@@ -168,7 +168,7 @@ final class WirexCore implements Wirex {
 
     private static final Logger LOG = LoggerFactory.getLogger(Wirex.class.getSimpleName());
 
-    public static final String version = "1.0.14.48-BETA";
+    public static final String version = "1.0.14.49-BETA";
 
     static {
         System.setProperty("org.apache.commons.logging.Log",
